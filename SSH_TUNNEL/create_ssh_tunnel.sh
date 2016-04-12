f895c983e8efbf3b320177835d628da27cf468f9
Basil Kurian <basil.kurian@aricent.com>
2016-04-12 14:52:53 +0530

    Added configurable SSH tunnel for maintainance page

--- SSH_TUNNEL/create_ssh_tunnel.sh
+++ SSH_TUNNEL/create_ssh_tunnel.sh
@@ -16,32 +16,46 @@ ACPU)
 	DESTINATION_MACHINE_SSH_PORT=$ACPUPORT
 	DESTINATION_MACHINE_TUNNEL_PORT=$ACPUPORT
 	DESTINATION_MACHINE_USER=$ACPUMACHINEUSER
-	PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
-	PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
-	echo "Use below ports for profiling"
-	echo "PCS - 9001"
-	echo "FCC - 9003"
-	echo "HA  - 9005"
+	if [ "$PROFILINGREQUIRED" == "YES" ]
+	then
+		PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
+		PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
+		
+		echo "Use below ports for profiling"
+		echo "PCS - 9001"
+		echo "FCC - 9003"
+		echo "HA  - 9005"
+	fi
+	
   ;;
 ACPU2P1)
 	DESTINATION_MACHINE_IP=$ACPUIP
 	DESTINATION_MACHINE_SSH_PORT=$ACPUPORT
 	DESTINATION_MACHINE_TUNNEL_PORT=$ACPUPORT
 	DESTINATION_MACHINE_USER=$ACPUMACHINEUSER
-	PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
-	PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
-	echo "Use below ports for profiling"
-	echo "PCS - 9001"
-	echo "FCC - 9003"
-	echo "HA  - 9005"
+	if [ "$PROFILINGREQUIRED" == "YES" ]
+	then
+		PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
+		PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002 -L9003:localhost:9003 -L9004:localhost:9004 -L9005:localhost:9005 -L9006:localhost:9006"
+		
+		echo "Use below ports for profiling"
+		echo "PCS - 9001"
+		echo "FCC - 9003"
+		echo "HA  - 9005"
+	fi
   ;;
 ACPU2P2)
 	DESTINATION_MACHINE_IP=$ACPUP2IP
 	DESTINATION_MACHINE_SSH_PORT=$ACPUP2PORT
 	DESTINATION_MACHINE_TUNNEL_PORT=$ACPUP2PORT
 	DESTINATION_MACHINE_USER=$ACPUMACHINEUSER
-	PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002"
-	PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002"
+	if [ "$PROFILINGREQUIRED" == "YES" ]
+	then
+		PROCESSTUNNEL1="-L9001:localhost:9001 -L9002:localhost:9002"
+		PROCESSTUNNEL2="-L9001:localhost:9001 -L9002:localhost:9002"
+		echo "Use below ports for profiling"
+		echo "FCC - 9001"
+	fi
   ;;
  *)
 	# 
@@ -62,6 +76,9 @@ if [ "$MAINTENANCEREQUIRED" == "YES" ]
 then
 	PROCESSTUNNEL1="$PROCESSTUNNEL1 -L8084:localhost:8084"
 	PROCESSTUNNEL2="$PROCESSTUNNEL2 -L8084:localhost:8084"
+	
+	echo "Use below ports for Maintenance page"
+	echo "Maintenance - 8084"
 fi
 
 #
